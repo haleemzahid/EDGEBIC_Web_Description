1af05513f9498ffdf122ddc35fcd9038b2de06a8
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2025-12-29 16:10:38 +0500

    refactor: remove commented-out code in HeroContent section for cleaner codebase

--- components/marketing/sections/hero-content-client.tsx
+++ components/marketing/sections/hero-content-client.tsx
@@ -113,9 +113,9 @@ export function HeroContent(): React.JSX.Element {
             </div>
 
             {/* Ratings/Reviews Section */}
-            <div className="flex flex-col items-center gap-3">
-              <div className="flex items-center justify-center gap-1">
-                {[...Array(4)].map((_, i) => (
+            {/* <div className="flex flex-col items-center gap-3"> */}
+            {/* <div className="flex items-center justify-center gap-1"> */}
+            {/* {[...Array(4)].map((_, i) => (
                   <svg
                     key={i}
                     className="size-5 fill-yellow-400"
@@ -123,9 +123,9 @@ export function HeroContent(): React.JSX.Element {
                   >
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                   </svg>
-                ))}
-                {/* Half star */}
-                <svg
+                ))} */}
+            {/* Half star */}
+            {/* <svg
                   className="size-5"
                   viewBox="0 0 20 20"
                 >
@@ -145,14 +145,13 @@ export function HeroContent(): React.JSX.Element {
                     fill="url(#halfStarGradient)"
                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
                   />
-                </svg>
-                <span className="ml-2 text-sm text-white">
+                </svg> */}
+            {/* <span className="ml-2 text-sm text-white">
                   Top-Rated Manufacturing Software
-                </span>
-              </div>
-              <div className="flex items-center justify-center gap-4">
-                {/* G2 Logo */}
-                <a
+                </span> */}
+            {/* </div> */}
+            {/* <div className="flex items-center justify-center gap-4"> */}
+            {/* <a
                   href="https://www.g2.com/products/resource-manager-db-rmdb/competitors/alternativesproducts/resource-manager-db-rmdb/competitors/alternatives"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -187,7 +186,6 @@ export function HeroContent(): React.JSX.Element {
                     />
                   </svg>
                 </a>
-                {/* Capterra Logo */}
                 <a
                   href="https://www.capterra.com/p/9402/Resource-Manager-DB/"
                   target="_blank"
@@ -223,7 +221,6 @@ export function HeroContent(): React.JSX.Element {
                     />
                   </svg>
                 </a>
-                {/* Software Advice Logo */}
                 <a
                   href="https://sourceforge.net/software/product/Resource-Manager-DB/#"
                   target="_blank"
@@ -333,9 +330,9 @@ export function HeroContent(): React.JSX.Element {
                       />
                     </g>
                   </svg>
-                </a>
-              </div>
-            </div>
+                </a> */}
+            {/* </div> */}
+            {/* </div> */}
           </motion.div>
         </div>
       </div>
