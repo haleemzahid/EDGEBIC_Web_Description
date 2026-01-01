dcc3c7083e23dd1263ccdd35aa47dbefe7a4bf0e
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2026-01-01 15:33:34 +0500

    feat: implement TestHomePage component with initial layout and sections

---
+++ app/(app)/(marketing)/test-home/page.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import type { Metadata } from 'next';
+
+import { AwardsSection } from '@/components/marketing/sections/awards-section';
+import { ManufacturingFeatureSection } from '@/components/marketing/sections/manufacturing-feature-section';
+import { NTClipboardHero } from '@/components/marketing/sections/ntclipboard-hero';
+import { NTClipboardSummary } from '@/components/marketing/sections/ntclipboard-summary';
+import { NTClipboardTestimonials } from '@/components/marketing/sections/ntclipboard-testimonials';
+import { NTClipboardToolBox } from '@/components/marketing/sections/ntclipboard-toolbox';
+import { ChallengesBenefitsSection } from '@/components/marketing/sections/challenges-benefits-section';
+
+export const metadata: Metadata = {
+  title: 'Test Home',
+  description:
+    'EDGEBIC production planning and scheduling software. Flexible, affordable solutions that adapt to your operations.'
+};
+
+export default function TestHomePage(): React.JSX.Element {
+  return (
+    <>
+      <NTClipboardHero />
+      <ChallengesBenefitsSection />
+      <AwardsSection />
+      {/* <NTClipboardSummary /> */}
+      <ManufacturingFeatureSection />
+      {/* <NTClipboardTestimonials /> */}
+      {/* <NTClipboardPricing /> */}
+      {/* <NTClipboardDemo /> */}
+      {/* <NTClipboardFAQ /> */}
+      {/* <NTClipboardCTA /> */}
+    </>
+  );
+}
