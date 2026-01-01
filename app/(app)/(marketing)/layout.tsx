14cfe61853e81ff101fe4871f84e19ddc2e097eb
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2026-01-01 14:52:24 +0500

    refactor: comment out BackToTop component for cleaner layout

--- app/(app)/(marketing)/layout.tsx
+++ app/(app)/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from '@/components/marketing/footer';
 import { CookieBanner } from '@/components/marketing/fragments/cookie-banner';
 import { Navbar } from '@/components/marketing/navbar';
 // import { AnnouncementBar } from '@/components/ui/announcement-bar';
-import { BackToTop } from '@/components/ui/back-to-top';
+// import { BackToTop } from '@/components/ui/back-to-top';
 import { FloatingCTA } from '@/components/ui/floating-cta';
 import { PageTransition } from '@/components/ui/page-transition';
 
@@ -19,7 +19,7 @@ export default function MarketingLayout(
       <PageTransition>{props.children}</PageTransition>
       <Footer />
       <CookieBanner />
-      <BackToTop />
+      {/* <BackToTop /> */}
       <FloatingCTA />
     </div>
   );
