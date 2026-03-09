08f673ac16f1848046f06244f733da6c7e88811e
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2026-03-09 11:17:55 +0500

    Refactor ValuesPage layout for improved responsiveness of the success stories image

--- app/(app)/(marketing)/values/page.tsx
+++ app/(app)/(marketing)/values/page.tsx
@@ -89,7 +89,7 @@ export default function ValuesPage(): React.JSX.Element {
               </div>
 
               {/* Right Column - Success Stories Image */}
-              <div className="relative  h-full w-full overflow-hidden rounded-lg shadow-lg lg:min-h-[500px]">
+              <div className="relative min-h-[300px] h-full w-full overflow-hidden rounded-lg shadow-lg lg:min-h-[500px]">
                 <Image
                   src="/value.jpeg"
                   alt="Success Stories - Our Values in Action"
