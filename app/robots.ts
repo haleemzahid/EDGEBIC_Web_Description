9e28f48e54268508de538dae139f6dd151bf93eb
haleemzahid <Haleemzahid35@gmail.com>
2026-03-16 23:34:15 +0500

    fix robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -35,7 +35,6 @@ export default function robots(): MetadataRoute.Robots {
           '/product-downloads-3',
           // Duplicate pages (canonical versions exist)
           '/aboutus',
-          '/success_stories',
           '/mission',
           // Hidden sections
           '/author/',
