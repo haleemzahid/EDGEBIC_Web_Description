19bf5c4f30b34025e6b1691e3b7843b06a63bdc2
mudasir Nadeem <mudasirnadeem7979@gmail.com>
2026-04-25 10:04:40 +0500

    Refactor SocialMediaLayout to remove AnnotatedSection and simplify rendering; update SocialMediaLoading to include AnnotatedSection for better structure.

--- app/(app)/dashboard/settings/organization/information/@socialMedia/loading.tsx
+++ app/(app)/dashboard/settings/organization/information/@socialMedia/loading.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 
 import { SocialMediaSkeletonCard } from '@/components/dashboard/settings/organization/information/social-media-skeleton-card';
+import { AnnotatedSection } from '@/components/ui/annotated';
 
 export default function SocialMediaLoading(): React.JSX.Element {
-  return <SocialMediaSkeletonCard />;
+  return (
+    <AnnotatedSection
+      title="YouTube Video"
+      description="Set the YouTube video that will be displayed on your homepage and admin dashboard. This video will be visible to all visitors."
+    >
+      <SocialMediaSkeletonCard />
+    </AnnotatedSection>
+  );
 }
